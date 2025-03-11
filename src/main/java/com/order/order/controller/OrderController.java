f68f2301b8dc4d6edf32e5430f3d5a523c07e755
wavier and pro <gkhari05@gmail.com>
2025-03-11 16:35:15 +0530

    first commit

---
+++ src/main/java/com/order/order/controller/OrderController.java
@@ -0,0 +1,17 @@
+package com.order.order.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+
+    @GetMapping("/message")
+    public String orderMessage(){
+        return "Message from Order Service HI.";
+    }
+
+}
